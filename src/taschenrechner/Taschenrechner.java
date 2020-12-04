2b643256b87db86ff6e840caaeabe62fa87116a8
Stefan Obergroebner <obergroebner@kabelmail.de>
2020-12-04 21:27:52 +0100

    Added subtract method.

--- src/taschenrechner/Taschenrechner.java
+++ src/taschenrechner/Taschenrechner.java
@@ -15,10 +15,23 @@ public class Taschenrechner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int zahl1 = 1;
+        int zahl2 = 2;
+
+        int result = add(zahl1, zahl2);
+        int result_sub = subtract(zahl1, zahl2);
+        
+
+        System.out.println(result);
+        System.out.println(result_sub);
+        
     }
-    
+
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
